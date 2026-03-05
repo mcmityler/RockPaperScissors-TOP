ba4f9cf942d69ee3022705b5cce0bc60de30be56
Tyler McMillan <36271467+mcmityler@users.noreply.github.com>
2026-03-05 09:28:56 -0500

    Add logic to for the computers hand choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+
+// create function to get a random hand from computer
+function getComputerChoice(){
+    // create a math random value between 0-2 (0,1, or 2)
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+    // depending on what number return a different string (rock, paper, or scissors)
+    return (randomNum === 1) ? "rock" :
+    (randomNum === 2) ? "paper" : "scissors";
+}
+
+console.log(getComputerChoice());
